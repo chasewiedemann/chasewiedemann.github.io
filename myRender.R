386777676697a71e691eff05c161e3431142ebc6
chasewiedemann <60712465+chasewiedemann@users.noreply.github.com>
2024-08-11 15:54:02 -0500

    This Script_ Update 2 081124

--- myRender.R
+++ myRender.R
@@ -2,6 +2,7 @@
 library(tools)
 all_files = list.files(paste0(getwd()),recursive = T)
 toRender = all_files[file_ext(all_files) == "Rmd"]
+toRender = toRender[toRender != "index.Rmd"] ### Because Render Site will do this
 
 for (files in toRender) {
   rmarkdown::render(files)  
