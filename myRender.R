d8a58839a52eaa2787bed66e651fda72cdc5cc61
chasewiedemann <60712465+chasewiedemann@users.noreply.github.com>
2024-08-11 16:48:49 -0500

    How to get Home unlabeled 1 081124

--- myRender.R
+++ myRender.R
@@ -2,7 +2,6 @@
 library(tools)
 all_files = list.files(paste0(getwd()),recursive = T)
 toRender = all_files[file_ext(all_files) == "Rmd"]
-toRender = toRender[toRender != "index.Rmd"] ### Because Render Site will do this
 
 
 
