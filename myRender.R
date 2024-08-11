0dd122fdafc5c571a4c60fdb737dbcbc1d1b75bd
chasewiedemann <60712465+chasewiedemann@users.noreply.github.com>
2024-08-11 15:14:26 -0500

    Oh No 081124

---
+++ myRender.R
@@ -0,0 +1,8 @@
+
+library(tools)
+all_files = list.files(paste0(getwd()),recursive = T)
+toRender = all_files[file_ext(all_files) == "Rmd"]
+
+for (files in toRender) {
+  rmarkdown::render(files)  
+}
