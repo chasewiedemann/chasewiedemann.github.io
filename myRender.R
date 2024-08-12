dc3592b129e3424eba4414f59af34e9413e7de45
chasewiedemann <60712465+chasewiedemann@users.noreply.github.com>
2024-08-12 09:55:07 -0500

    Site Refinements _ Update 081224

--- myRender.R
+++ myRender.R
@@ -1,13 +1,13 @@
 
 library(tools)
-rmarkdown::render_site()
-all_files = list.files(paste0(getwd()),recursive = T)
-toRender = all_files[file_ext(all_files) == "Rmd"]
 
+all_files = list.files(paste0(getwd()),recursive = T)
+toRender = all_files[file_ext(all_files) == "Rmd" & !grepl("_Template",all_files)]
+unlink("_site",recursive = T)
 
 
 for (files in toRender) {
     rmarkdown::render(files)
 }
 
-unlink("_site",recursive = T)
+
