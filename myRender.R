c12325e8564e1a8891ed8135fcd06b311a96c105
chasewiedemann <60712465+chasewiedemann@users.noreply.github.com>
2024-08-13 16:40:26 -0500

    fixed eval problem

--- myRender.R
+++ myRender.R
@@ -1,6 +1,8 @@
 
 library(tools)
 
+knitr::opts_chunk$set(eval = T)
+
 all_files = list.files(paste0(getwd()),recursive = T)
 toRender = all_files[file_ext(all_files) == "Rmd" & !grepl("_Template",all_files)]
 unlink("_site",recursive = T)
