0324550ff6d7a7fe3259f8d79d1657258a924c69
chasewiedemann <60712465+chasewiedemann@users.noreply.github.com>
2024-08-14 16:27:32 -0500

    last commit of the day!

--- myRender.R
+++ myRender.R
@@ -5,7 +5,6 @@ knitr::opts_chunk$set(eval = T)
 
 all_files = list.files(paste0(getwd()),recursive = T)
 toRender = all_files[file_ext(all_files) == "Rmd" & !grepl("_Template",all_files)]
-unlink("_site",recursive = T)
 
 
 for (files in toRender) {
